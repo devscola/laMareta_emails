efb9462823ee6f662ae950f9c5025773fe1a0063
Javier Aristizabal <javieraristizabal@gmail.com>
2015-05-12 12:09:03 +0200

    Include Birthday helper

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require './helpers/birthday'
 require './models/vip_clients.rb'
 require './models/invitations.rb'
 
+include Birthday
+
 DataMapper.setup(:default, ENV['DATABASE_URL'])
 
 get '/' do
@@ -14,6 +16,6 @@ get '/' do
 end
 
 def send_invitations
- return greetings if @vip_clients.any?
+ return Birthday.greetings if @vip_clients.any?
 end
 
