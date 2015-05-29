b4f3022bad1ab41045d337073494a10f2e0c2927
Javier Aristizabal <javieraristizabal@gmail.com>
2015-05-29 16:25:27 +0200

    Create the class LaMaretaEmails

--- app.rb
+++ app.rb
@@ -6,17 +6,21 @@ require './helpers/birthday'
 require './models/vip_clients.rb'
 require './models/invitations.rb'
 
-include Birthday
+class LaMaretaEmails < Sinatra::Base
 
-configure :development, :test do
-  DataMapper.setup(:default, 'postgres://postgres@localhost/usersmareta')
-end
+  include Birthday
 
-configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
+  configure :development, :test do
+    DataMapper.setup(:default, 'postgres://postgres@localhost/usersmareta')
+  end
 
-def send_emails
-  vip_clients = VipClients.all
-  Birthday.greetings(vip_clients)
+  configure :production do
+    DataMapper.setup(:default, ENV['DATABASE_URL'])
+  end
+
+  get '/' do
+    vip_clients = VipClients.all
+    Birthday.greetings(vip_clients)
+    halt 200
+  end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,4 +1,4 @@
 desc 'This task is called by the Heroku scheduler add-on'
 task :scheduler do
-  send_emails
+  LaMaretaEmails.run!
 end
